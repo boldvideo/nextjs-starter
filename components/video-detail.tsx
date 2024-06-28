28bccc570908200bb60aaac1e02617076e897bd8
Marcel Fahle <m.fahle@gmail.com>
2024-06-28 06:21:17 +0200

    playerpage cleanup

--- components/video-detail.tsx
+++ components/video-detail.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import clsx from "clsx";
 import { formatRelative } from "date-fns";
 import { Player } from "components/player";
 import { Transcript } from "@/components/transcript";
@@ -15,6 +16,8 @@ type Chapter = {
 };
 
 const parseChapters = (webvttString: string): Chapter[] => {
+  if (!webvttString) return [];
+  if (!webvttString.includes("WEBVTT")) return [];
   const lines = webvttString.split("\n\n").slice(1); // Skip the WEBVTT header and split chapters
   return lines.map((line) => {
     const [_identifier, timeRange, title] = line.split("\n");
@@ -70,26 +73,74 @@ const timestampToSeconds = (timestamp: string): number => {
 
 export function VideoDetail({ video }: VideoDetailProps) {
   const playerRef = useRef<HTMLVideoElement>(null);
-  const [parsedChapters, setParsedChapters] = useState<Chapter[]>([]);
-
-  useEffect(() => {
-    const chaptersList = video.chapters ? parseChapters(video.chapters) : [];
-    setParsedChapters(chaptersList);
-  }, [video]);
 
   const handleCueClick = (time: number) => {
     const toTime = isNaN(time) ? 0.1 : parseFloat(time.toString());
-    console.log("playerRef", playerRef?.current);
     if (playerRef?.current) {
       playerRef.current.currentTime = toTime;
       playerRef.current.play();
     }
   };
 
+  const chapters = parseChapters(video.chapters || "");
+  const hasChapters = chapters && chapters.length > 0;
+
   return (
     <div className="flex flex-col pb-60">
-      <div className="z-40 relative -mx-5 md:-mx-10 -mt-5 md:-mt-10 mb-5 md:mb-10 flex justify-center bg-black w-screen aspect-video max-h-[calc(100vh-240px)]">
-        <Player video={video} autoPlay={true} ref={playerRef} />
+      <div
+        className={clsx(
+          "w-full lg:max-h-[75vh]",
+          hasChapters && "lg:grid lg:grid-cols-12 lg:space-y-0",
+          "overflow-hidden border-b border-b-ct-dark",
+        )}
+      >
+        <div className="bg-black w-full flex-grow aspect-video col-span-9">
+          <Player video={video} autoPlay={true} ref={playerRef} />
+        </div>
+        {hasChapters && (
+          <div className="relative flex flex-col col-span-3 overflow-y-scroll text-gray-700">
+            <div className="lg:absolute top-0 left-0 w-full h-full flex flex-col">
+              <h3 className="p-3 font-bold text-lg ">Chapters</h3>
+              <ol>
+                {chapters.map((chapter, idx) => (
+                  <li
+                    key={chapter.startTime}
+                    onClick={() =>
+                      handleCueClick(timestampToSeconds(chapter.startTime))
+                    }
+                  >
+                    <div className="group cursor-pointer flex space-x-3 p-3 font-semibold hover:bg-ct-yellow">
+                      <div className="flex items-start">
+                        <div className="w-6 leading-5 pt-px text-xs opacity-60 group-hover:opacity-100 font-normal tracking-tight">
+                          {idx + 1}
+                        </div>
+                      </div>
+                      <div className="relative w-20 h-12 aspect-video flex-shrink-0 overflow-hidden border border-gray-300">
+                        <Image
+                          src={`https://image.mux.com/${video.playback_id}/thumbnail.png?width=400&height=200&fit_mode=smartcrop&time=${timestampToSeconds(chapter.startTime)}`}
+                          alt={chapter.title}
+                          fill={true}
+                          style={{ objectFit: "cover" }}
+                          className=""
+                        />
+                      </div>
+                      <div className="flex flex-col">
+                        <div className="w-full leading-tight">
+                          {chapter.title}
+                        </div>
+                        <div>
+                          <span className="text-gray-400 text-xs">
+                            {chapter.startTime}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ol>
+            </div>
+          </div>
+        )}
       </div>
       <div className="md:max-w-4xl w-full mx-auto px-5 md:px-10">
         <h1 className="text-3xl md:text-[42px] font-extrabold mb-4 leading-tight">
@@ -102,7 +153,7 @@ export function VideoDetail({ video }: VideoDetailProps) {
 
         {false && video.chapters && (
           <div className="mb-12 text-[21px]">
-            {parsedChapters.map((chapter, index) => (
+            {chapters.map((chapter, index) => (
               <div
                 key={index}
                 className="cursor-pointer"
@@ -116,35 +167,6 @@ export function VideoDetail({ video }: VideoDetailProps) {
                 {chapter.title}
               </div>
             ))}
-          </div>
-        )}
-        {video.chapters && (
-          <div className="w-full overflow-x-auto">
-            <div className="flex mb-12 text-sm space-x-4">
-              {parsedChapters.map((chapter, index) => (
-                <div
-                  key={index}
-                  className="shrink-0 cursor-pointer w-40"
-                  onClick={() =>
-                    handleCueClick(timestampToSeconds(chapter.startTime))
-                  }
-                >
-                  <Image
-                    src={`https://image.mux.com/${video.playback_id}/thumbnail.png?width=400&height=200&fit_mode=smartcrop&time=${timestampToSeconds(chapter.startTime)}`}
-                    alt={chapter.title}
-                    width={160}
-                    height={80}
-                    className="rounded-sm overflow-hidden"
-                  />
-                  <div className="px-1 pt-2">
-                    <span className="text-xs px-1 py-0.5 rounded-lg bg-yellow-200 text-yellow-800 ">
-                      {chapter.startTime}
-                    </span>
-                    <h4 className="w-full font-bold mt-4">{chapter.title}</h4>
-                  </div>
-                </div>
-              ))}
-            </div>
           </div>
         )}
       </div>
